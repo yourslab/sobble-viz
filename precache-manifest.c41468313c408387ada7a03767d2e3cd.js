ce7f94a33f9bf2be84e46e38a7238221e1f34f7b
yourslab <abrina@usc.edu>
2019-11-20 01:10:33 -0800

    Updates

---
+++ precache-manifest.c41468313c408387ada7a03767d2e3cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db6bb38e12b098744b6fccd6736f8fc",
+    "url": "/viz-app/index.html"
+  },
+  {
+    "revision": "2e4192c9e656f34c94f2",
+    "url": "/viz-app/static/css/2.7058fc60.chunk.css"
+  },
+  {
+    "revision": "e8f12e20cf656bc9968c",
+    "url": "/viz-app/static/css/main.bf5f1ced.chunk.css"
+  },
+  {
+    "revision": "2e4192c9e656f34c94f2",
+    "url": "/viz-app/static/js/2.2ada188b.chunk.js"
+  },
+  {
+    "revision": "e8f12e20cf656bc9968c",
+    "url": "/viz-app/static/js/main.2561cd20.chunk.js"
+  },
+  {
+    "revision": "539371e9abbe46290bfb",
+    "url": "/viz-app/static/js/runtime-main.90dcc614.js"
+  },
+  {
+    "revision": "2d6d79545cc033821147866e0fa25f5f",
+    "url": "/viz-app/static/media/r3_nice.2d6d7954.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/r3/precache-manifest.558ff149821a4222be17e3ff91af6042.js"
+  "/viz-app/precache-manifest.c41468313c408387ada7a03767d2e3cd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/r3/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/viz-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
