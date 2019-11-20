57d6377d0b5a91b1d8bd9484171526f658af70bd
yourslab <abrina@usc.edu>
2019-11-20 01:06:51 -0800

    Updates

---
+++ precache-manifest.558ff149821a4222be17e3ff91af6042.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e63838a8a5adaf6eb92fa4b7457c58f",
+    "url": "/r3/index.html"
+  },
+  {
+    "revision": "21c08fb5d2f5b55a685d",
+    "url": "/r3/static/css/2.7058fc60.chunk.css"
+  },
+  {
+    "revision": "e8f12e20cf656bc9968c",
+    "url": "/r3/static/css/main.bf5f1ced.chunk.css"
+  },
+  {
+    "revision": "21c08fb5d2f5b55a685d",
+    "url": "/r3/static/js/2.8b76a21b.chunk.js"
+  },
+  {
+    "revision": "e8f12e20cf656bc9968c",
+    "url": "/r3/static/js/main.2561cd20.chunk.js"
+  },
+  {
+    "revision": "3a253bf823da8d0174c8",
+    "url": "/r3/static/js/runtime-main.ad82e84d.js"
+  },
+  {
+    "revision": "2d6d79545cc033821147866e0fa25f5f",
+    "url": "/r3/static/media/r3_nice.2d6d7954.png"
+  }
+]);
